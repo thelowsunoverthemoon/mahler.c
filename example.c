ceb61a6d88f6cc8e1650e9be510110813fbdf2a0
thelowsunoverthemoon <48923877+thelowsunoverthemoon@users.noreply.github.com>
2020-08-27 19:02:18 -0700

    Update example.c

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 
 int main(void) {
     Note scbase[8];
-    Scale cmajor = getScale((Note) {C, NONE, 4}, &NATURALMINSCALE, scbase, DESCEND);
+    Scale cmajor = getScale((Note) {C, NONE, 4}, &NATURAL_MIN_SCALE, scbase, DESCEND);
     printf("Major Scale and Chord of each note of the C4 minor scale, descending, along with its frequency and wavelength\n\n");
 
     Note scdisp[8];
@@ -12,8 +12,8 @@ int main(void) {
     for (int i = 0; i < cmajor.size; i++) {
         printNote("Major Scale of ", cmajor.notes[i], " ");
         printf("(Frequency : %f - Wavelength : %f)\n", getFreqOrWave(cmajor.notes[i], STANDARD, FREQUENCY), getFreqOrWave(cmajor.notes[i], STANDARD, WAVELENGTH));
-        printScale("", getScale(cmajor.notes[i], &MAJORSCALE, scdisp, ASCEND), "\n");
+        printScale("", getScale(cmajor.notes[i], &MAJOR_SCALE, scdisp, ASCEND), "\n");
         printNote("Major Triad of ", cmajor.notes[i], "\n");
-        printChord("", getChord(cmajor.notes[i], &MAJORTRIAD, chdbase, chdnotes), "\n\n");
+        printChord("", getChord(cmajor.notes[i], &MAJOR_TRIAD, chdbase, chdnotes), "\n\n");
     }
 }
