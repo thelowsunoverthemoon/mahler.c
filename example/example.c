3f435cbc87a8b031a20c028a2554a98512b8844a
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2021-09-06 10:47:56 -0700

    Update example.c

--- example/example.c
+++ example/example.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include "musictheory.h"
+#include <stdlib.h>
+#include "mahler.h"
 
 int
 main(void) {
+    
     struct Note scNotes[8];
-    struct Scale scale = getScale((struct Note) {C, NONE, 0}, &MAJOR_SCALE, scNotes, SCALE_ASCEND);
+    struct Scale scale = getScale((struct Note) {MAHLER_C, MAHLER_NONE, 0}, &MAHLER_MAJOR_SCALE, scNotes, MAHLER_ASCEND);
 
     struct Note tempNotes[8];
     for (size_t i = 0; i < scale.size; i++) {
-        struct Scale temp = getScale(scale.notes[i], &MAJOR_SCALE, tempNotes, SCALE_ASCEND);
+        struct Scale temp = getScale(scale.notes[i], &MAHLER_MAJOR_SCALE, tempNotes, MAHLER_ASCEND);
         
-        char disp[MT_DISP_LEN];
+        char disp[MAHLER_DISP_LEN];
         for (size_t j = 0; j < scale.size; j++) {
-            printf("%s ", printNote(temp.notes[j], disp, MT_DISP_LEN));
+            printf("%s ", printNote(temp.notes[j], disp, MAHLER_DISP_LEN));
         }
         putchar('\n');
     }
