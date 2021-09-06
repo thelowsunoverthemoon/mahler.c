bc32091679e82c9694e18be6146c0f7a6fac60a2
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2021-09-06 10:46:42 -0700

    Update and rename musictheory.c to mahler.c

--- src/mahler.c
+++ src/mahler.c
@@ -7,32 +7,32 @@
 #define SIMPLE_INTER_MAX 8      // Simple Interval maximum
 
 #define RETURN_ERROR(A, B) {\
-    MT_LAST_ERROR = B;      \
+    MAHLER_LAST_ERROR = B;      \
     return (struct A) {0};  \
 }
 
 // Enumerators //
 
 enum NoteOrder {
-    C, D, E, F, G, A, B
+    MAHLER_C, MAHLER_D, MAHLER_E, MAHLER_F, MAHLER_G, MAHLER_A, MAHLER_B
 };
 
 enum Quality {
-    MINOR = -1, MAJOR = 0, AUGMENTED = 1, DIMINISHED = -2, PERFECT = 3
+    MAHLER_MINOR = -1, MAHLER_MAJOR = 0, MAHLER_AUGMENTED = 1, MAHLER_DIMINISHED = -2, MAHLER_PERFECT = 3
 };
 
 enum Accidental {
-    DBFLAT = -2, FLAT = -1, NONE = 0, SHARP = 1, DBSHARP = 2
+    MAHLER_DBFLAT = -2, MAHLER_FLAT = -1, MAHLER_NONE = 0, MAHLER_SHARP = 1, MAHLER_DBSHARP = 2
 };
 
 enum ScaleType {
-    SCALE_ASCEND, SCALE_DESCEND, SCALE_FULL
+    MAHLER_ASCEND, MAHLER_DESCEND, MAHLER_FULL
 };
 
-enum MT_Error { // change
-    MT_ERROR_NONE,
-    MT_ERROR_INVALID_QUAL, MT_ERROR_INVALID_INTER, MT_ERROR_INVALID_INVERSION, MT_ERROR_INVALID_PRINT_NOTE,
-    MT_ERROR_OVERFLOW_PRINT_NOTE, MT_ERROR_OVERFLOW_SCALE_RETURN, MT_ERROR_OVERFLOW_CHORD_RETURN
+enum MahlerError { // ERROR prefix to differentiate
+    MAHLER_ERROR_NONE,
+    MAHLER_ERROR_INVALID_QUAL, MAHLER_ERROR_INVALID_INTER, MAHLER_ERROR_INVALID_INVERSION, MAHLER_ERROR_INVALID_PRINT_NOTE,
+    MAHLER_ERROR_OVERFLOW_PRINT_NOTE, MAHLER_ERROR_OVERFLOW_SCALE_RETURN, MAHLER_ERROR_OVERFLOW_CHORD_RETURN
 };
 
 // Structures //
@@ -101,7 +101,7 @@ void returnScale(struct Note const notes[], size_t noteNum, struct ScaleResult l
 
 // Misc Functions //
 
-char* getMTError(void);
+char* getMahlerError(void);
 char* printNote(struct Note const note, char buf[], size_t size);
 bool isEnharmonic(struct Note notea, struct Note noteb);
 
@@ -113,73 +113,73 @@ static inline bool matchNote(struct Note const notes[], size_t noteNum, struct N
 
 // Preset Chords & Scales //
 
-struct ChordBase const MAJOR_TRIAD = {
-    "Major Triad", 3, (struct Interval[]) {{3, MAJOR},{3, MINOR}}
+struct ChordBase const MAHLER_MAJOR_TRIAD = {
+    "Major Triad", 3, (struct Interval[]) {{3, MAHLER_MAJOR},{3, MAHLER_MINOR}}
 };
 
-struct ChordBase const MINOR_TRIAD = {
-    "Minor Triad", 3, (struct Interval[]) {{3, MINOR},{3, MAJOR}}
+struct ChordBase const MAHLER_MINOR_TRIAD = {
+    "Minor Triad", 3, (struct Interval[]) {{3, MAHLER_MINOR},{3, MAHLER_MAJOR}}
 };
 
-struct ChordBase const AUGMENTED_TRIAD = {
-    "Augmented Triad", 3, (struct Interval[]) {{3, MAJOR},{3, MAJOR}}
+struct ChordBase const MAHLER_AUGMENTED_TRIAD = {
+    "Augmented Triad", 3, (struct Interval[]) {{3, MAHLER_MAJOR},{3, MAHLER_MAJOR}}
 };
 
-struct ChordBase const DIMINISHED_TRIAD = {
-    "Diminished Triad", 3, (struct Interval[]) {{3, MINOR},{3, MINOR}}
+struct ChordBase const MAHLER_DIMINISHED_TRIAD = {
+    "Diminished Triad", 3, (struct Interval[]) {{3, MAHLER_MINOR},{3, MAHLER_MINOR}}
 };
 
-struct ChordBase const DIMINISHED_7 = {
-    "Diminished 7th", 4, (struct Interval[]) {{3, MINOR},{3, MINOR},{3, MINOR}}
+struct ChordBase const MAHLER_DIMINISHED_7 = {
+    "Diminished 7th", 4, (struct Interval[]) {{3, MAHLER_MINOR},{3, MAHLER_MINOR},{3, MAHLER_MINOR}}
 };
 
-struct ChordBase const HALF_DIMINISHED_7 = {
-    "Half-Diminished 7th", 4, (struct Interval[]) {{3, MINOR},{3, MINOR},{3, MAJOR}}
+struct ChordBase const MAHLER_HALF_DIMINISHED_7 = {
+    "Half-Diminished 7th", 4, (struct Interval[]) {{3, MAHLER_MINOR},{3, MAHLER_MINOR},{3, MAHLER_MAJOR}}
 };
 
-struct ChordBase const MINOR_7 = {
-    "Minor 7th", 4, (struct Interval[]) {{3, MINOR},{3, MAJOR},{3, MINOR}}
+struct ChordBase const MAHLER_MINOR_7 = {
+    "Minor 7th", 4, (struct Interval[]) {{3, MAHLER_MINOR},{3, MAHLER_MAJOR},{3, MAHLER_MINOR}}
 };
 
-struct ChordBase const MAJOR_7 = {
-    "Major 7th", 4, (struct Interval[]) {{3, MAJOR},{3, MINOR},{3, MAJOR}}
+struct ChordBase const MAHLER_MAJOR_7 = {
+    "Major 7th", 4, (struct Interval[]) {{3, MAHLER_MAJOR},{3, MAHLER_MINOR},{3, MAHLER_MAJOR}}
 };
 
-struct ChordBase const DOMINANT_7 = {
-    "Dominant 7th", 4, (struct Interval[]) {{3, MAJOR},{3, MINOR},{3, MINOR}}
+struct ChordBase const MAHLER_DOMINANT_7 = {
+    "Dominant 7th", 4, (struct Interval[]) {{3, MAHLER_MAJOR},{3, MAHLER_MINOR},{3, MAHLER_MINOR}}
 };
 
-struct ScaleBase const MAJOR_SCALE = {
-    "Major", 8, (struct Interval[]) {{2, MAJOR},{2, MAJOR},{2, MINOR},{2, MAJOR},{2, MAJOR},{2, MAJOR},{2, MINOR}}
+struct ScaleBase const MAHLER_MAJOR_SCALE = {
+    "Major", 8, (struct Interval[]) {{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MINOR}}
 };
 
-struct ScaleBase const NATURAL_MIN_SCALE = {
-    "Natural Minor", 8, (struct Interval[]) {{2, MAJOR},{2, MINOR},{2, MAJOR},{2, MAJOR},{2, MINOR},{2, MAJOR},{2, MAJOR}}
+struct ScaleBase const MAHLER_NATURAL_MIN_SCALE = {
+    "Natural Minor", 8, (struct Interval[]) {{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR}}
 };
 
-struct ScaleBase const HARMONIC_MIN_SCALE = {
-    "Harmonic Minor", 8, (struct Interval[]) {{2, MAJOR},{2, MINOR},{2, MAJOR},{2, MAJOR},{2, MINOR},{2, AUGMENTED},{2, MINOR}}
+struct ScaleBase const MAHLER_HARMONIC_MIN_SCALE = {
+    "Harmonic Minor", 8, (struct Interval[]) {{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_AUGMENTED},{2, MAHLER_MINOR}}
 };
 
-struct ScaleBase const MELODIC_MIN_SCALE = {
-    "Melodic Minor", 8, (struct Interval[]) {{2, MAJOR},{2, MINOR},{2, MAJOR},{2, MAJOR},{2, MAJOR},{2, MAJOR},{2, MINOR}}
+struct ScaleBase const MAHLER_MELODIC_MIN_SCALE = {
+    "Melodic Minor", 8, (struct Interval[]) {{2, MAHLER_MAJOR},{2, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{2, MAHLER_MINOR}}
 };
 
-struct ScaleBase const PENTATONIC_MIN_SCALE = {
-    "Pentatonic", 6, (struct Interval[]) {{2, MAJOR},{2, MAJOR},{3, MINOR},{2, MAJOR},{3, MINOR}}
+struct ScaleBase const MAHLER_PENTATONIC_MIN_SCALE = {
+    "Pentatonic", 6, (struct Interval[]) {{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{3, MAHLER_MINOR},{2, MAHLER_MAJOR},{3, MAHLER_MINOR}}
 };
 
-struct ScaleBase const PENTATONIC_MAJ_SCALE = {
-    "Pentatonic Major", 6, (struct Interval[]) {{3, MINOR},{2, MAJOR},{2, MAJOR},{3, MINOR},{2, MAJOR}}
+struct ScaleBase const MAHLER_PENTATONIC_MAJ_SCALE = {
+    "Pentatonic Major", 6, (struct Interval[]) {{3, MAHLER_MINOR},{2, MAHLER_MAJOR},{2, MAHLER_MAJOR},{3, MAHLER_MINOR},{2, MAHLER_MAJOR}}
 };
 
-struct ScaleBase const BLUES_SCALE = {
-    "Blues", 7, (struct Interval[]) {{3, MINOR}, {2, MAJOR}, {1, AUGMENTED}, {2, MINOR}, {3, MINOR}, {2, MAJOR}}
+struct ScaleBase const MAHLER_BLUES_SCALE = {
+    "Blues", 7, (struct Interval[]) {{3, MAHLER_MINOR}, {2, MAHLER_MAJOR}, {1, MAHLER_AUGMENTED}, {2, MAHLER_MINOR}, {3, MAHLER_MINOR}, {2, MAHLER_MAJOR}}
 };
 
 // Internal Global Variables //
 
-static enum MT_Error MT_LAST_ERROR = MT_ERROR_NONE;
+static enum MahlerError MAHLER_LAST_ERROR = MAHLER_ERROR_NONE;
 
 static int const INTER_STEPS[] = {
     0, 2, 4, 5, 7, 9, 11, 12
@@ -188,7 +188,7 @@ static int const INTER_STEPS[] = {
 static inline int
 toSemitone(int note)
 {
-    return note * 2 + (note <= E);  // "if" accounts for F (we convert from C, D, E, to C, C#, D, ect)
+    return note * 2 + (note <= MAHLER_E);  // "if" accounts for F (we convert from C, D, E, to C, C#, D, ect)
 }
 
 char*
@@ -198,17 +198,17 @@ printNote(struct Note const note, char buf[], size_t size)
     static char const* const dispAccidental[] = {"bbbb", "bbb", "bb", "b", "", "#", "##", "###", "####"};
     
     if (note.acci < -4 || note.acci > 4) { // dispAccidental limit
-        MT_LAST_ERROR = MT_ERROR_INVALID_PRINT_NOTE;
+        MAHLER_LAST_ERROR = MAHLER_ERROR_INVALID_PRINT_NOTE;
         return "";
     }
     
-    if (note.note < C || note.note > B) {
-        MT_LAST_ERROR = MT_ERROR_INVALID_PRINT_NOTE;
+    if (note.note < MAHLER_C || note.note > MAHLER_B) {
+        MAHLER_LAST_ERROR = MAHLER_ERROR_INVALID_PRINT_NOTE;
         return "";
     }
     
     if (!(snprintf(buf, size, "%s%s%d", dispNote[note.note], dispAccidental[note.acci + 4], note.pitch) < size)) {
-        MT_LAST_ERROR = MT_ERROR_OVERFLOW_PRINT_NOTE;
+        MAHLER_LAST_ERROR = MAHLER_ERROR_OVERFLOW_PRINT_NOTE;
     }
     return buf;
 }
@@ -217,7 +217,7 @@ void
 invertChord(struct Chord* chord, int const inversion)
 {
     if (inversion >= chord->size || inversion < 0) {
-        MT_LAST_ERROR = MT_ERROR_INVALID_INVERSION;
+        MAHLER_LAST_ERROR = MAHLER_ERROR_INVALID_INVERSION;
         return;
     }
     memcpy(chord->notes, chord->base, sizeof(struct Note) * chord->size);
@@ -266,13 +266,13 @@ returnInter(struct Note const notea, struct Note const noteb)
     
     int quality = (toSemitone(noteb.note) + noteb.acci + pitch * SIZE_CHROMATIC) - (toSemitone(notea.note) + notea.acci + notea.pitch * SIZE_CHROMATIC) - INTER_STEPS[inter - 1];
     if (quality < -2 || quality > 1) {
-        RETURN_ERROR(Interval, MT_ERROR_INVALID_INTER);
+        RETURN_ERROR(Interval, MAHLER_ERROR_INVALID_INTER);
     }
     if (inter == 1 || inter == 4 || inter == 5 || inter == 8) {
-        if (quality == MAJOR) {
-            quality = PERFECT;
-        } else if (quality == MINOR) {
-            quality = DIMINISHED;
+        if (quality == MAHLER_MAJOR) {
+            quality = MAHLER_PERFECT;
+        } else if (quality == MAHLER_MINOR) {
+            quality = MAHLER_DIMINISHED;
         }
     }
     
@@ -294,21 +294,21 @@ getInter(struct Note const note, struct Interval const interval)
     int end = 0;
     if (inter == 1 || inter == 4 || inter == 5 || inter == 8) { // if perfect interval
         switch (quality) {
-            case AUGMENTED:
+            case MAHLER_AUGMENTED:
                 end = 0;
                 break;
-            case DIMINISHED:
+            case MAHLER_DIMINISHED:
                 end = 1;
                 break;
-            case PERFECT:
+            case MAHLER_PERFECT:
                 end = -3;
                 break;
             default:
-                RETURN_ERROR(Note, MT_ERROR_INVALID_QUAL);
+                RETURN_ERROR(Note, MAHLER_ERROR_INVALID_QUAL);
         }
     } else {
-        if (quality == PERFECT) {
-            RETURN_ERROR(Note, MT_ERROR_INVALID_QUAL);
+        if (quality == MAHLER_PERFECT) {
+            RETURN_ERROR(Note, MAHLER_ERROR_INVALID_QUAL);
         }
     }
     
@@ -318,7 +318,7 @@ getInter(struct Note const note, struct Interval const interval)
         above = true;
         pitch += 1;
     }
-    end += toSemitone(root) + INTER_STEPS[inter - 1] + note.acci + quality - 1; // INTER_STEPS is amount of steps for major interval
+    end += toSemitone(root) + INTER_STEPS[inter - 1] + note.acci + quality - 1; // INTER_STEPS is amount of steps for MAHLER_MAJOR interval
     int acci = above ? end - (toSemitone(enharmonic) - 3) - (enharmonic <= 9) : end - (toSemitone(enharmonic) - 1); // 7 -> 9 is C to E (see toSemitone)
     return (struct Note) {
         enharmonic - (above * ADJUST_NOTE),
@@ -331,10 +331,10 @@ void
 returnScale(struct Note const notes[], size_t noteNum, struct ScaleResult list[], size_t listMax, bool useEnharmonic)
 {
     static struct ScaleBase const* scalePos[] = {
-        &MAJOR_SCALE,
-        &NATURAL_MIN_SCALE,
-        &HARMONIC_MIN_SCALE,
-        &MELODIC_MIN_SCALE
+        &MAHLER_MAJOR_SCALE,
+        &MAHLER_NATURAL_MIN_SCALE,
+        &MAHLER_HARMONIC_MIN_SCALE,
+        &MAHLER_MELODIC_MIN_SCALE
     };
     static struct Note scaleArray[8];
     static scalePosNum = sizeof(scalePos) / sizeof(struct ScaleBase*);
@@ -344,23 +344,23 @@ returnScale(struct Note const notes[], size_t noteNum, struct ScaleResult list[]
         if (scalePos[i]->size - 1 < noteNum) {
             continue;
         }
-        for (enum NoteOrder n = C; n <= B; n++) {
-            for (enum Accidental a = FLAT; a <= SHARP; a++) {
+        for (enum NoteOrder n = MAHLER_C; n <= MAHLER_B; n++) {
+            for (enum Accidental a = MAHLER_FLAT; a <= MAHLER_SHARP; a++) {
                 size_t checkNote = noteNum;
                 struct Scale scale = getScale(
-                    (struct Note) {n, a, 0}, scalePos[i], scaleArray, SCALE_ASCEND
+                    (struct Note) {n, a, 0}, scalePos[i], scaleArray, MAHLER_ASCEND
                 );
                 if (matchNote(notes, noteNum, scale.notes, scale.size)) {
                     if (listNum == listMax - 1) {
-                        MT_LAST_ERROR = MT_ERROR_OVERFLOW_SCALE_RETURN;
+                        MAHLER_LAST_ERROR = MAHLER_ERROR_OVERFLOW_SCALE_RETURN;
                         goto exit;
                     }
                     list[listNum++] = (struct ScaleResult) {
                         (struct Note) {n, a, 0}, scalePos[i]
                     };
-                    if (useEnharmonic && a != NONE) { // must be flat or sharp
+                    if (useEnharmonic && a != MAHLER_NONE) { // must be flat or sharp
                         if (listNum == listMax - 1) {
-                            MT_LAST_ERROR = MT_ERROR_OVERFLOW_SCALE_RETURN;
+                            MAHLER_LAST_ERROR = MAHLER_ERROR_OVERFLOW_SCALE_RETURN;
                             goto exit;
                         }
                         list[listNum++] = (struct ScaleResult) {
@@ -380,12 +380,12 @@ void
 returnChord(struct Note const notes[], size_t noteNum, struct ChordResult list[], size_t listMax, bool useEnharmonic)
 {
     static struct ChordBase const* chordPos[] = {
-        &MAJOR_TRIAD,
-        &MINOR_TRIAD,
-        &AUGMENTED_TRIAD,
-        &DIMINISHED_TRIAD,
-        &DIMINISHED_7,
-        &DOMINANT_7
+        &MAHLER_MAJOR_TRIAD,
+        &MAHLER_MINOR_TRIAD,
+        &MAHLER_AUGMENTED_TRIAD,
+        &MAHLER_DIMINISHED_TRIAD,
+        &MAHLER_DIMINISHED_7,
+        &MAHLER_DOMINANT_7
     };
     static struct Note chordBase[4];
     static struct Note chordNotes[4];
@@ -396,23 +396,23 @@ returnChord(struct Note const notes[], size_t noteNum, struct ChordResult list[]
         if (chordPos[i]->size - 1 < noteNum) {
             continue;
         }
-        for (enum NoteOrder n = C; n <= B; n++) {
-            for (enum Accidental a = FLAT; a <= SHARP; a++) {
+        for (enum NoteOrder n = MAHLER_C; n <= MAHLER_B; n++) {
+            for (enum Accidental a = MAHLER_FLAT; a <= MAHLER_SHARP; a++) {
                 size_t checkNote = noteNum;
                 struct Chord chord = getChord(
                     (struct Note) {n, a, 0}, chordPos[i], chordBase, chordNotes
                 );
                 if (matchNote(notes, noteNum, chord.notes, chord.size)) {
                     if (listNum == listMax - 1) {
-                        MT_LAST_ERROR = MT_ERROR_OVERFLOW_CHORD_RETURN;
+                        MAHLER_LAST_ERROR = MAHLER_ERROR_OVERFLOW_CHORD_RETURN;
                         goto exit;
                     }
                     list[listNum++] = (struct ChordResult) {
                         (struct Note) {n, a, 0}, chordPos[i]
                     };
-                    if (useEnharmonic && a != NONE) { // must be flat or sharp
+                    if (useEnharmonic && a != MAHLER_NONE) { // must be flat or sharp
                         if (listNum == listMax - 1) {
-                            MT_LAST_ERROR = MT_ERROR_OVERFLOW_CHORD_RETURN;
+                            MAHLER_LAST_ERROR = MAHLER_ERROR_OVERFLOW_CHORD_RETURN;
                             goto exit;
                         }
                         list[listNum++] = (struct ChordResult) {
@@ -433,15 +433,15 @@ getEnharmonic(struct Note const note) // only works for sharps and flats
 {
     enum NoteOrder tone = note.note;
     int enharmonic = tone + note.acci;
-    if (enharmonic > B) {
-        enharmonic = C;
-    } else if (enharmonic < C) {
-        enharmonic = B;
+    if (enharmonic > MAHLER_B) {
+        enharmonic = MAHLER_C;
+    } else if (enharmonic < MAHLER_C) {
+        enharmonic = MAHLER_B;
     }
     enum Accidental acci = note.acci;
     return (struct Note) {
         enharmonic,
-        -acci * ((acci == SHARP && tone != B && tone != E) || (acci == FLAT && tone != C && tone != F)),
+        -acci * ((acci == MAHLER_SHARP && tone != MAHLER_B && tone != MAHLER_E) || (acci == MAHLER_FLAT && tone != MAHLER_C && tone != MAHLER_F)),
         0
     };
 }
@@ -467,26 +467,26 @@ getScale(struct Note const start, struct ScaleBase const* type, struct Note note
 {
     int size = type->size;
     switch(mode) {
-        case SCALE_DESCEND:
+        case MAHLER_DESCEND:
             notes[type->size - 1] = start;
             break;
-        case SCALE_ASCEND:
+        case MAHLER_ASCEND:
             notes[0] = start;
             break;
-        case SCALE_FULL:
+        case MAHLER_FULL:
             size *= 2;
             notes[0] = notes[type->size * 2 - 1] = start;
             break;
     }
     for (size_t i = 1; i < type->size; i++) {
         switch(mode) {
-            case SCALE_DESCEND:
+            case MAHLER_DESCEND:
                 notes[type->size - i - 1] = getInter(notes[type->size - i], type->steps[i - 1]);
                 break;
-            case SCALE_ASCEND:
+            case MAHLER_ASCEND:
                 notes[i] = getInter(notes[i - 1], type->steps[i - 1]);
                 break;
-            case SCALE_FULL:
+            case MAHLER_FULL:
                 notes[i] = notes[type->size * 2 - i - 1] = getInter(notes[i - 1], type->steps[i - 1]);
                 break;
         }
@@ -505,23 +505,25 @@ isEnharmonic(struct Note const noteA, struct Note const noteB)
 }
 
 char*
-getMTError(void)
+getMAHLERError(void)
 {
-    switch (MT_LAST_ERROR) {
-        case MT_ERROR_NONE:
+    switch (MAHLER_LAST_ERROR) {
+        case MAHLER_ERROR_NONE:
             return "None";
-        case MT_ERROR_INVALID_QUAL:
+        case MAHLER_ERROR_INVALID_QUAL:
             return "Invalid Interval with Perfect Quality";
-        case MT_ERROR_INVALID_INTER:
+        case MAHLER_ERROR_INVALID_INTER:
             return "Notes produce Invalid Interval";
-        case MT_ERROR_INVALID_INVERSION:
+        case MAHLER_ERROR_INVALID_INVERSION:
             return "Invalid Inversion for Chord";
-        case MT_ERROR_INVALID_PRINT_NOTE:
+        case MAHLER_ERROR_INVALID_PRINT_NOTE:
             return "Out of Range Accidental for Print Note";
-        case MT_ERROR_OVERFLOW_PRINT_NOTE:
+        case MAHLER_ERROR_OVERFLOW_PRINT_NOTE:
             return "Note Text is too Large";
-        case MT_ERROR_OVERFLOW_SCALE_RETURN:
+        case MAHLER_ERROR_OVERFLOW_SCALE_RETURN:
             return "Too many Scale Return Results";
+        case MAHLER_ERROR_OVERFLOW_CHORD_RETURN:
+            return "Too many Chord Return Results";
         default:
             return "Unknown Error";
     }
