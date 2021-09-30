4dca46ae9fe39db93276f83db99b0e32c1d3fbd3
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2021-09-29 18:43:24 -0700

    Update example.c

--- example/example.c
+++ example/example.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 #include "mahler.h"
 
+// Create a Major Scale Chart starting from C //
+
 int
 main(void) {
-    
     struct Note scNotes[8];
-    struct Scale scale = getScale((struct Note) {MAHLER_C, MAHLER_NONE, 0}, &MAHLER_MAJOR_SCALE, scNotes, MAHLER_ASCEND);
+    struct Scale scale = getScale(
+        (struct Note) {MAHLER_C, MAHLER_NONE, 0}, &MAHLER_MAJOR_SCALE, scNotes, MAHLER_ASCEND
+    );
 
     struct Note tempNotes[8];
     for (size_t i = 0; i < scale.size; i++) {
-        struct Scale temp = getScale(scale.notes[i], &MAHLER_MAJOR_SCALE, tempNotes, MAHLER_ASCEND);
+        struct Scale temp = getScale(
+            scale.notes[i], &MAHLER_MAJOR_SCALE, tempNotes, MAHLER_ASCEND
+        );
         
         char disp[MAHLER_DISP_LEN];
         for (size_t j = 0; j < scale.size; j++) {
-            printf("%s ", printNote(temp.notes[j], disp, MAHLER_DISP_LEN));
+            printf("%-3s ", printNote(temp.notes[j], disp, MAHLER_DISP_LEN));
         }
         putchar('\n');
     }
