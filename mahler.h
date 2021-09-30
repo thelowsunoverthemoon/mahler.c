43a6371936c8c860f26a4450bd5d18316d99dac9
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2021-09-29 18:27:54 -0700

    Update mahler.h

--- mahler.h
+++ mahler.h
@@ -7,23 +7,46 @@
 
 // Enumerators //
 
-enum NoteOrder {
+enum MahlerNote {
     MAHLER_C, MAHLER_D, MAHLER_E, MAHLER_F, MAHLER_G, MAHLER_A, MAHLER_B
 };
 
-enum Quality {
+enum MahlerQuality {
     MAHLER_MINOR = -1, MAHLER_MAJOR = 0, MAHLER_AUGMENTED = 1, MAHLER_DIMINISHED = -2, MAHLER_PERFECT = 3
 };
 
-enum Accidental {
+enum MahlerAcci {
     MAHLER_DBFLAT = -2, MAHLER_FLAT = -1, MAHLER_NONE = 0, MAHLER_SHARP = 1, MAHLER_DBSHARP = 2
 };
 
-enum ScaleType {
+enum MahlerKeyType {
+    MAHLER_MAJOR_KEY, MAHLER_MINOR_KEY
+};
+
+enum MahlerScaleType {
     MAHLER_ASCEND, MAHLER_DESCEND, MAHLER_FULL
 };
 
-enum MAHLER_Error {
+enum MahlerDegree {
+    MAHLER_TONIC = 1, MAHLER_SUPERTONIC = 2, MAHLER_MEDIANT = 3, MAHLER_SUBDOMINANT = 4,
+    MAHLER_DOMINANT = 5, MAHLER_SUBMEDIANT = 6, MAHLER_LEADING_TONE = 7, MAHLER_SUBTONIC = 8
+};
+
+enum MahlerNumeral {
+    MAHLER_I = 1, MAHLER_II = 2, MAHLER_III = 3, MAHLER_IV = 4,
+    MAHLER_V = 5, MAHLER_VI = 6, MAHLER_VII = 7, MAHLER_VIII = 8
+};
+
+enum MahlerMode {
+    MAHLER_IONIAN = 1, MAHLER_DORIAN = 2, MAHLER_PHRYGIAN = 3, MAHLER_LYDIAN = 4,
+    MAHLER_MIXOLYDIAN = 5, MAHLER_AEOLIAN = 6, MAHLER_LOCRIAN = 7
+};
+
+enum MahlerChordType {
+     MAHLER_TRIAD = 3, MAHLER_SEVENTH_CHORD = 4, MAHLER_NINTH_CHORD = 5, MAHLER_ELEVENTH_CHORD = 6
+};
+
+enum MahlerError {
     MAHLER_ERROR_NONE,
     MAHLER_ERROR_INVALID_QUAL, MAHLER_ERROR_INVALID_INTER, MAHLER_ERROR_INVALID_INVERSION, MAHLER_ERROR_INVALID_PRINT_NOTE,
     MAHLER_ERROR_OVERFLOW_PRINT_NOTE, MAHLER_ERROR_OVERFLOW_SCALE_RETURN, MAHLER_ERROR_OVERFLOW_CHORD_RETURN
@@ -32,21 +55,21 @@ enum MAHLER_Error {
 // Structures //
 
 struct Note {
-    enum NoteOrder  note;
-    enum Accidental acci;
+    enum MahlerNote note;
+    int             acci; // use int instead? Since only guaranteed enum to hold char, could have way higher flats
     int             pitch;
 };
 
 struct Interval {
-    int          inter;
-    enum Quality quality;
+    int                inter;
+    enum MahlerQuality quality;
 };
 
 struct Chord {
-    int const                         size;
-    int                               inversion;
-    struct Note const* const restrict base;
-    struct Note* const restrict       notes;
+    int const                size;
+    int                      inversion;
+    struct Note const* const base; // include restrict?...
+    struct Note* const       notes;
 };
 
 struct ChordResult {
@@ -60,9 +83,16 @@ struct ChordBase {
     struct Interval const* steps;
 };
 
+struct ChordList {
+    struct ChordBase const** chordPos;
+    size_t const             chordNum;
+    struct Note* restrict    chordBase;
+    struct Note* restrict    chordNotes;
+};
+
 struct Scale {
     int const            size;
-    enum ScaleType const type;
+    enum MahlerScaleType const type;
     struct Note* const   notes;
 };
 
@@ -77,6 +107,20 @@ struct ScaleBase {
     struct Interval const* steps;
 };
 
+struct ScaleList {
+    struct ScaleBase const** scalePos;
+    size_t const             scaleNum;
+    struct Note*             scaleArray;
+};
+
+struct KeySig {
+    enum MahlerKeyType const type;
+    int const                alter; // alteration
+    int const                size;
+    struct Note const        key;
+    struct Note              notes[7];
+};
+
 // Preset Chords & Scales //
 
 extern struct ScaleBase const MAHLER_MAJOR_SCALE;          // Major Scale
@@ -86,6 +130,8 @@ extern struct ScaleBase const MAHLER_MELODIC_MIN_SCALE;    // Melodic Minor Scal
 extern struct ScaleBase const MAHLER_PENTATONIC_MAJ_SCALE; // Major Pentatonic Scale
 extern struct ScaleBase const MAHLER_PENTATONIC_MIN_SCALE; // Minor Pentatonic Scale
 extern struct ScaleBase const MAHLER_BLUES_SCALE;          // Blues Scale (hexatonic)
+extern struct ScaleBase const MAHLER_OCTATONIC_HALF_SCALE; // Octatonic Scale (starting with half tone)
+extern struct ScaleBase const MAHLER_OCTATONIC_WHOLE_SCALE;// Octatonic Scale (starting with whole tone)
 
 extern struct ChordBase const MAHLER_MAJOR_TRIAD;          // Major Triad
 extern struct ChordBase const MAHLER_MINOR_TRIAD;          // Minor Triad
@@ -100,23 +146,32 @@ extern struct ChordBase const MAHLER_DOMINANT_7;           // Dominant 7th
 // Interval Functions //
 
 struct Note getInter(struct Note note, struct Interval interval);
-struct Interval returnInter(struct Note notea, struct Note noteb);
+struct Interval returnInter(struct Note noteA, struct Note noteB);
 
 // Chord Functions //
 
+struct Chord getKeyChord(struct KeySig const* key, size_t index, size_t size, struct Note* restrict base, struct Note* restrict notes);
 struct Chord getChord(struct Note root, struct ChordBase const* type, struct Note* restrict base, struct Note* restrict notes);
-void returnChord(struct Note const notes[], size_t noteNum, struct ChordResult list[], size_t listMax, bool useEnharmonic);
+void returnChord(struct Note const notes[], size_t noteNum, struct ChordResult list[], size_t listMax, struct ChordList const* custom, bool enharmonic);
 void invertChord(struct Chord* chord, int inversion);
 
 // Scale Functions //
 
-struct Scale getScale(struct Note start, const struct ScaleBase* type, struct Note notes[], enum ScaleType mode);
-void returnScale(struct Note const note[], size_t noteNum, struct ScaleResult list[], size_t listMax, bool useEnharmonic);
+struct Scale getKeyScale(struct KeySig const* key, size_t index, enum MahlerScaleType mode, struct ScaleBase const* type, struct Note* notes);
+struct Scale getScale(struct Note start, const struct ScaleBase* type, struct Note notes[], enum MahlerScaleType mode);
+void returnScale(struct Note const notes[], size_t noteNum, struct ScaleResult list[], size_t listMax, struct ScaleList const* custom, bool enharmonic);
+
+// Key Signature Functions //
+
+struct KeySig getKeySig(struct Note key, enum MahlerKeyType type);
+struct KeySig returnKeySig(char const* str, enum MahlerKeyType type);
+struct KeySig getKeyRelative(struct KeySig const* key);
+int queryAcci(struct KeySig const* key, enum MahlerNote note);
 
 // Misc Functions //
 
-char* getMAHLERError(void);
+char* getMahlerError(void);
 char* printNote(struct Note const note, char buf[], size_t size);
-bool isEnharmonic(struct Note noteA, struct Note noteB);
+bool isEnharmonic(struct Note notea, struct Note noteb);
 
 #endif
